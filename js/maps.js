9f4aa735f3e015cbc5f870f65bfbe3aa0871624f
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2023-05-19 15:33:25 +0200

    rettelser js
    dd

--- js/maps.js
+++ js/maps.js
@@ -1,14 +1,63 @@
-let map;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 55.39594, lng: 10.38831 },
-    zoom: 12,
-    disableDefaultUI: true,
-    zoomControl: true,
-    fullscreenControl: true,
-  });
+
+async function initMap(){
+
+  // Map option
+    var options = {
+      center: { lat: 55.39594, lng: 10.38831 },
+      zoom: 12,
+      disableDefaultUI: true,
+      zoomControl: true,
+      fullscreenControl: true,
+    }
+
+    // New Map
+    map = new google.maps.Map(document.getElementById("map"), options)
+
+
+    function addMarker(property){
+      
+      const marker = new google.maps.Marker ({
+        position:property.location,
+        map:map,
+        icon: blob,
+      });
+  
+  }
+  
+  addMarker({location:{ lat: 55.384090, lng: 10.363820 }});
+
+
+
 }
 
-initMap();
+
+
+
+
+
+
+
+
+
+
+
+
+// Add marker
+/*
+function addMarker(location){
+    
+    const marker = new google.maps.Marker ({
+      position:location,
+      map:map
+
+    });
+
+}
+
+addMarker({location:{ lat: 55.39769847267258, lng: 10.380654906127582 }});
+
+
+*/
+
+
