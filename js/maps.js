dc0a6578b603cb6bae73f974d42ddffe4f788a38
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2023-05-21 16:58:25 +0200

    added js code
    dd

--- js/maps.js
+++ js/maps.js
@@ -16,48 +16,92 @@ async function initMap(){
 
 
 
+   // Keep track of the currently opened info window
+    let currentInfoWindow = null;
 
-    // Add marker
-    function addMarker(property){
-      
-      const marker = new google.maps.Marker ({
-        position:property.location,
-        map:map,
-        icon: property.imageIcon,
-        
-      });
+   // Add marker
+    function addMarker(property) {
+  const marker = new google.maps.Marker({
+    position: property.location,
+    map: map,
+    icon: property.imageIcon,
+  });
 
-      // Add Window
-
+   // Add Window
       const detailWindow = new google.maps.InfoWindow({
-        content: property.content
-      })
+    content: property.content,
+  });
+
+   // Add click event for markers to display infowindow
+     marker.addListener("click", () => {
+   // Close the currently opened info window, if any
+    if (currentInfoWindow) {
+      currentInfoWindow.close();
+    }
+
+    // Open the new info window
+    detailWindow.open(map, marker);
+
+    // Set the newly opened info window as the current info window
+    currentInfoWindow = detailWindow;
+  });
   
-      // Add click event for markers to display infowindow
-
-      marker.addListener("click", () =>{
-        detailWindow.open(map, marker);
-      })
-
+  
+      
 
   }
 
-  // Add markers with content
+  // Add markers with content , 
   
-  addMarker({location:{ lat: 55.384090, lng: 10.363820 },
+  addMarker({location:{ lat: 55.39435162532834, lng: 10.356862676330612 },          // Lyn 1 - CCS-Combo Circle K
   imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
   content:
-  '<div id="content" style="width: 150px; height: 200px">'+
+  '<div id="content" style="width: 200px; height: 220px">'+
     '<div id="bodyContent>'+
-      '<div id="adr" style="width: 140px;"><h3>Adresse som er mega sej og nice og den kan alt muligt sejt</h3></div>'+
+      '<div id="adr" style="width: 180px;"><h3>Middelfartvej 53, 5200 Odense</h3></div>'+
       '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b> CCS-Combo</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Antal:</b> 2</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Hastighedstype:</b> Lyn&nbsp;<span style="font-size: 12px; color: rgb(126, 126, 126);">(150kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> Circle K</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
+  addMarker({location:{ lat: 55.37786938684984, lng: 10.431300556754865 },          // Lyn 2 - CCS-Combo CLEVER
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 200px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Niels Bohrs Alle 150, 5230 Odense</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b> CCS-Combo</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Antal:</b> 2</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Hastighedstype:</b> Lyn&nbsp;<span style="font-size: 12px; color: rgb(126, 126, 126);">(150kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> CLEVER</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
+  addMarker({location:{ lat: 55.354487237305484, lng: 10.423615957124706 },          // Lyn 3 - CCS-Combo OK & Vestas
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 200px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Glisholmvej 5, 5260 Odense</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b> CCS-Combo</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Antal:</b> 6</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Hastighedstype:</b> Lyn&nbsp;<span style="font-size: 12px; color: rgb(126, 126, 126);">(300kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> OK</p></div>'+
     '</div>'+
   '</div>'
   
   });
 
 
-
 }
 
 
@@ -72,21 +116,6 @@ async function initMap(){
 
 
 
-// Add marker
-/*
-function addMarker(location){
-    
-    const marker = new google.maps.Marker ({
-      position:location,
-      map:map
 
-    });
-
-}
-
-addMarker({location:{ lat: 55.39769847267258, lng: 10.380654906127582 }});
-
-
-*/
 
 
