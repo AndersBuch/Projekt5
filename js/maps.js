55496e4e9a3ea588be74f133a48838c9a4f8493e
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2023-05-23 09:15:34 +0200

    Added more Locations - Js

--- js/maps.js
+++ js/maps.js
@@ -117,6 +117,88 @@ async function initMap(){
   
   });
 
+  addMarker({location:{ lat: 55.394407739170646, lng: 10.376135322645586 },          // Hurtig 2 - CCS-Combo, Type 2, CHAdeMO CLEVER
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 200px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Vesterbro 39, 5000 Odense</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;">CHAdeMO&nbsp<span style="font-size: 12px; color: rgb(126, 126, 126);">(1 stk, 50kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;">CCS-Combo&nbsp<span style="font-size: 12px; color: rgb(126, 126, 126);">(1 stk, 50kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;">Type 2&nbsp<span style="font-size: 12px; color: rgb(126, 126, 126);">(4 stk, 22kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> CLEVER</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
+  addMarker({location:{ lat: 55.354045, lng: 10.420364 },          // Hurtig 3 - CCS-Combo, CCS-Combo, CHAdeMO Allego
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 200px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Odense S 1, Drejebænken 1, 5260 Odense</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;">CHAdeMO&nbsp<span style="font-size: 12px; color: rgb(126, 126, 126);">(2 stk, 50kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;">CCS-Combo&nbsp<span style="font-size: 12px; color: rgb(126, 126, 126);">(2 stk, 50kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;">CCS-Combo&nbsp<span style="font-size: 12px; color: rgb(126, 126, 126);">(2 stk, 300kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> Allego</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
+  addMarker({location:{ lat: 55.3978544420754, lng: 10.370497209216277 },          // Normal 1 - Type 2 CLEVER  , 
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 210px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Ørstedsgade 30, 5000 Odense</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b> Type 2 - Hun</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Antal:</b> 4</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Hastighedstype:</b> Normal&nbsp;<span style="font-size: 12px; color: rgb(126, 126, 126);">(22kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> CLEVER</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
+  addMarker({location:{ lat: 55.40209195760249, lng: 10.406961983126832 },          // Normal 2 - Type 2 Sperto
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 210px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Østerlunden 5, 5000 Odense</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b> Type 2 - Hun</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Antal:</b> 6</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Hastighedstype:</b> Normal&nbsp;<span style="font-size: 12px; color: rgb(126, 126, 126);">(22kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> Sperto</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
+  addMarker({location:{ lat: 55.39208314775112, lng: 10.407762651050376 },          // Normal 3 - Type 2 OK
+  imageIcon: "https://buchdesigns.dk/img/baseline_location_pin_black_36dp.png",
+  content:
+  '<div id="content" style="width: 210px; height: 220px">'+
+    '<div id="bodyContent>'+
+      '<div id="adr" style="width: 180px;"><h3>Rødegårdsvej 84, 5000 Odense C</h3></div>'+
+      '<hr></hr>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Stiktype:</b> Type 2 - Hun</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Antal:</b> 4</p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Hastighedstype:</b> Normal&nbsp;<span style="font-size: 12px; color: rgb(126, 126, 126);">(22kW)</span></p></div>'+
+      '<div><p id="mapinfo" style="font-size: 15px;"><b>Udbyder:</b> OK</p></div>'+
+    '</div>'+
+  '</div>'
+  
+  });
+
 }
 
 
