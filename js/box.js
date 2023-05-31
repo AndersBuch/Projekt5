bd62d06135b310da9b71b698abc6b46fbcd8294f
nittayamatet <119924064+nittayamatet@users.noreply.github.com>
2023-05-31 12:13:27 +0200

    d

--- js/box.js
+++ js/box.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll',checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
-  const triggerBottom = window.innerHeight / 8 * 7;
+  const triggerBottom = window.innerHeight / 5 * 4;
 
   boxes.forEach((box) => {
     const boxTop =box.getBoundingClientRect().top;
